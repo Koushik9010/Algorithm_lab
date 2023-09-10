d8f4e6e34345dd5c4cd8869bb1327ede640ed9dc
Koushik9010 <123968866+Koushik9010@users.noreply.github.com>
2023-09-10 21:54:14 +0600

    bfs.c

--- bfs.c
+++ bfs.c
@@ -1,83 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VERTICES 10 // Corrected the definition
+#define MAX_VERTICES 10
 
-int matrix[MAX_VERTICES][MAX_VERTICES];
+int graph[MAX_VERTICES][MAX_VERTICES];
 int visited[MAX_VERTICES];
-int queue[MAX_VERTICES];
 int n;
-int front = -1, rear = -1;
 
-// Function to create a graph
-void createGraph() { // Renamed the function for consistency
+void createGraph() {
     printf("Enter the number of vertices: ");
     scanf("%d", &n);
 
-    printf("Enter the adjacency matrix: \n");
+    printf("Enter the adjacency matrix:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            scanf("%d", &matrix[i][j]);
+            scanf("%d", &graph[i][j]);
         }
     }
-
-    printf("\nThe graph is created...."); // Corrected the message
+    printf("Graph created.\n");
 }
 
-// Enqueue an element into the queue
-void enqueue(int vertex) {
-    if (rear == MAX_VERTICES - 1) {
-        printf("\nQueue is full");
-    } else {
-        if (front == -1) {
-            front = 0;
-        }
-        queue[++rear] = vertex;
-    }
-}
-
-// Dequeue an element from the queue
-int dequeue() {
-    if (front == -1) {
-        printf("\nQueue is empty.");
-        return -1; // Added a return value to handle the empty queue case
-    } else {
-        int item = queue[front++];
-        if (front > rear) {
-            front = rear = -1;
-        }
-        return item;
-    }
-}
-
-// Traversal
 void bfs(int start) {
-    printf("BFS traversal starts from vertex %d: \n", start + 1);
-    enqueue(start);
-    visited[start] = 1; // Corrected the initialization to 1
+    int queue[MAX_VERTICES];
+    int front = 0, rear = 0;
 
-    while (front != -1) {
-        int currentVertex = dequeue();
-        printf("%d ", currentVertex + 1);
+    printf("BFS traversal starting from vertex %d:\n", start + 1);
+
+    visited[start] = 1;
+    printf("%d ", start + 1);
+    queue[rear++] = start;
+
+    while (front < rear) {
+        int currentVertex = queue[front++];
 
         for (int i = 0; i < n; i++) {
-            if (matrix[currentVertex][i] == 1 && visited[i] == 0) {
-                enqueue(i);
+            if (graph[currentVertex][i] == 1 && !visited[i]) {
                 visited[i] = 1;
+                printf("%d ", i + 1);
+                queue[rear++] = i;
             }
         }
     }
-    printf("\n\n");
+
+    printf("\n");
 }
 
 int main() {
     int choice, start;
+
     do {
-        printf("\n**********MENU**********\n");
-        printf("1. Create graph\n");
-        printf("2. BFS traversal\n");
+        printf("************ MENU ************\n");
+        printf("1. Create Graph\n");
+        printf("2. BFS Traversal\n");
         printf("3. Exit\n");
-        printf("Enter your choice: "); // Corrected the prompt message
+        printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -86,27 +62,27 @@ int main() {
                 break;
             case 2:
                 if (n == 0) {
-                    printf("\nPlease create the graph first\n");
+                    printf("Please create a graph first.\n");
                 } else {
-                    printf("\nEnter the starting vertex for bfs: ");
+                    printf("Enter the starting vertex for BFS: ");
                     scanf("%d", &start);
                     if (start >= 1 && start <= n) {
-                        for (int i = 0; i < MAX_VERTICES; i++) {
+                        for (int i = 0; i < n; i++) {
                             visited[i] = 0;
                         }
-                        start = start - 1;
-                        bfs(start);
+                        bfs(start - 1);
                     } else {
-                        printf("\nInvalid starting vertex.");
+                        printf("Invalid starting vertex.\n");
                     }
                 }
                 break;
             case 3:
-                printf("\nExiting...\n"); // Corrected the exit message
+                printf("Exiting...\n");
                 break;
             default:
-                printf("\nInvalid choice. Please try again.\n");
+                printf("Invalid choice. Please try again.\n");
         }
     } while (choice != 3);
+
     return 0;
 }
